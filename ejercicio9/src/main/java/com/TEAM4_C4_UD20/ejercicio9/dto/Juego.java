1174fc64395b0f304465fd87e7b3ba7afb91475e
Eloi Martorell Martin <eloi.martin174@gmail.com>
2022-05-09 19:28:03 +0200

    view actualizada

--- ejercicio9/src/main/java/com/TEAM4_C4_UD20/ejercicio9/dto/Juego.java
+++ ejercicio9/src/main/java/com/TEAM4_C4_UD20/ejercicio9/dto/Juego.java
@@ -2,6 +2,21 @@ package com.TEAM4_C4_UD20.ejercicio9.dto;
 
 public class Juego {
 	
+	//cargar colores
 	
+	//al pulsar cambia de color, hay una parjea para cada una
+	
+	//el jugador solo puede mostrar a la vez
+	
+	//si son del mismo color desaparecen de la pantalla con setVisible(false)
+	
+	//si las dos cartas son del mismo color, vuelven a girarse
+	
+	//ADICIONAL
+	//usar imagenes
+	
+	//contador de intentos
+	
+	//MessageDialog de enhorabuena
 
 }
--- ejercicio9/src/main/java/com/TEAM4_C4_UD20/ejercicio9/view/View.java
+++ ejercicio9/src/main/java/com/TEAM4_C4_UD20/ejercicio9/view/View.java
@@ -1,16 +1,16 @@
 package com.TEAM4_C4_UD20.ejercicio9.view;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JToggleButton;
 import javax.swing.border.EmptyBorder;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 
 public class View extends JFrame {
 
@@ -19,26 +19,49 @@ public class View extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	
+	int ROWS = 4;
+	int COLS = 4;
+	//creamos un array bidimensional de botones
+	static JToggleButton btn[][] = new JToggleButton[4][4];
+	
 	public View() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 480, 480);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(4,4,4,4));	//grid X Y 4X4
+		contentPane.setLayout(new GridLayout(4,4,4,4));	//grid (graella) X Y 4X4
 		
-		//creamos un array
-		JButton btn[] = new JButton[16];
+		ActionListener girar = new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				for (int i = 0; i < ROWS; i++) {
+					for (int j = 0; j < COLS; j++) {
+						if(btn[i][j] == e.getSource()) {
+							//funcion(i,j)
+						}
+					}
+				}
+				
+			}
+		};
+		
+		//1) Botones JToggleButton perfectamente distribuidos
+		
 		//añadimos la imagen
 		ImageIcon dorso = new ImageIcon("resources/dorso.png");
 		//con un for creamos los botones
 		for (int i = 0; i < btn.length; i++) {
-			btn[i] = new JButton(dorso);
-			btn[i].setPreferredSize(new Dimension(120, 120));
-			
-			contentPane.add(btn[i]);
+			for (int j = 0; j < btn[i].length; j++) {
+				btn[i][j] = new JToggleButton(dorso);
+				btn[i][j].setPreferredSize(new Dimension(120, 120));
+				contentPane.add(btn[i][j]);
+				btn[i][j].addActionListener(girar);
+			}
 		}
 		
 	}
-
 }
