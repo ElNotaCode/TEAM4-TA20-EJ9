34992c9fe7f633c01d80246af8ccd9ca7fec438f
Eloi Martorell Martin <eloi.martin174@gmail.com>
2022-05-09 18:40:51 +0200

    view basica

--- ejercicio9/src/main/java/com/TEAM4_C4_UD20/ejercicio9/view/View.java
+++ ejercicio9/src/main/java/com/TEAM4_C4_UD20/ejercicio9/view/View.java
@@ -1,11 +1,16 @@
 package com.TEAM4_C4_UD20.ejercicio9.view;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 
 public class View extends JFrame {
 
@@ -16,11 +21,24 @@ public class View extends JFrame {
 	 */
 	public View() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 480, 480);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(4,4,4,4));	//grid X Y 4X4
+		
+		//creamos un array
+		JButton btn[] = new JButton[16];
+		//añadimos la imagen
+		ImageIcon dorso = new ImageIcon("resources/dorso.png");
+		//con un for creamos los botones
+		for (int i = 0; i < btn.length; i++) {
+			btn[i] = new JButton(dorso);
+			btn[i].setPreferredSize(new Dimension(120, 120));
+			
+			contentPane.add(btn[i]);
+		}
+		
 	}
 
 }
